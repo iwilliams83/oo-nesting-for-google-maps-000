6b5bec3b07cdb066389bebcfefdad196fc24d168
iwilliams83 <indirajamurath@gmail.com>
2017-11-28 21:02:02 +0000

    automatically backed up by learn

--- lib/location_maker.rb
+++ lib/location_maker.rb
@@ -1,3 +1,10 @@
 class LocationMaker
   @@google_maps_array = []
+
+  def add_location_to_array(location,latitude,longitude)
+    arr = []
+    arr << location 
+    arr << latitude
+    arr << longitude
+  end
 end
